d81a4b653256e1773f45a7529ca0f0c6b5d2692f
ramjipasricha <dptramji@gmail.com>
2026-04-03 09:30:08 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // This line ensures your assets (CSS/JS) load correctly on GitHub Pages
+  base: "./", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
